2bd067b06c90f844b998e99177cd914d431cc1ca
Vasyl Sokolyk <vsokolyk@gmail.com>
2025-05-31 22:44:54 +0300

    Use space indentation across all files

--- src/worker.ts
+++ src/worker.ts
@@ -4,34 +4,34 @@ import { BunRuntime, BunWorker } from "@effect/platform-bun";
 import { Console, Context, Effect, Layer, Stream } from "effect";
 
 class MyWorkerPool extends Context.Tag("@app/MyWorkerPool")<
-	MyWorkerPool,
-	Worker.WorkerPool<number, never, number>
+  MyWorkerPool,
+  Worker.WorkerPool<number, never, number>
 >() {}
 
 const PoolLive = Worker.makePoolLayer(MyWorkerPool, {
-	size: OS.availableParallelism(),
+  size: OS.availableParallelism(),
 }).pipe(
-	Layer.provide(
-		BunWorker.layer(
-			() => new globalThis.Worker(`${__dirname}/worker/range.ts`),
-		),
-	),
+  Layer.provide(
+    BunWorker.layer(
+      () => new globalThis.Worker(`${__dirname}/worker/range.ts`),
+    ),
+  ),
 );
 
 Effect.gen(function* () {
-	const pool = yield* MyWorkerPool;
-	yield* Effect.all(
-		[
-			pool
-				.execute(5)
-				.pipe(Stream.runForEach((_) => Console.log("worker 1", _))),
-			pool
-				.execute(10)
-				.pipe(Stream.runForEach((_) => Console.log("worker 2", _))),
-			pool
-				.execute(15)
-				.pipe(Stream.runForEach((_) => Console.log("worker 3", _))),
-		],
-		{ concurrency: "inherit" },
-	);
+  const pool = yield* MyWorkerPool;
+  yield* Effect.all(
+    [
+      pool
+        .execute(5)
+        .pipe(Stream.runForEach((_) => Console.log("worker 1", _))),
+      pool
+        .execute(10)
+        .pipe(Stream.runForEach((_) => Console.log("worker 2", _))),
+      pool
+        .execute(15)
+        .pipe(Stream.runForEach((_) => Console.log("worker 3", _))),
+    ],
+    { concurrency: "inherit" },
+  );
 }).pipe(Effect.provide(PoolLive), BunRuntime.runMain);
--- src/worker/service.ts
+++ src/worker/service.ts
@@ -117,43 +117,43 @@
 // );
 
 import {
-	FetchHttpClient,
-	HttpClient,
-	HttpClientRequest,
-	HttpClientResponse,
+  FetchHttpClient,
+  HttpClient,
+  HttpClientRequest,
+  HttpClientResponse,
 } from "@effect/platform";
 import { Console, Effect, Layer, Schema } from "effect";
 
 const Post = Schema.Struct({
-	id: Schema.Number,
-	title: Schema.String,
+  id: Schema.Number,
+  title: Schema.String,
 });
 
 const CustomFetchLive = FetchHttpClient.layer.pipe(
-	Layer.provide(
-		Layer.succeed(FetchHttpClient.RequestInit, {
-			verbose: true,
-			tls: {
-				rejectUnauthorized: false,
-			},
-		} as BunFetchRequestInit),
-	),
+  Layer.provide(
+    Layer.succeed(FetchHttpClient.RequestInit, {
+      verbose: true,
+      tls: {
+        rejectUnauthorized: false,
+      },
+    } as BunFetchRequestInit),
+  ),
 );
 
 const program = Effect.gen(function* () {
-	const client = (yield* HttpClient.HttpClient).pipe(
-		HttpClient.tapRequest(Console.log),
-		HttpClient.filterStatusOk,
-		HttpClient.mapRequest(
-			HttpClientRequest.prependUrl("https://jsonplaceholder.typicode.com"),
-		),
-	);
+  const client = (yield* HttpClient.HttpClient).pipe(
+    HttpClient.tapRequest(Console.log),
+    HttpClient.filterStatusOk,
+    HttpClient.mapRequest(
+      HttpClientRequest.prependUrl("https://jsonplaceholder.typicode.com"),
+    ),
+  );
 
-	const response = yield* client.get("/posts/1");
+  const response = yield* client.get("/posts/1");
 
-	return yield* HttpClientResponse.schemaBodyJson(Post)(response);
-	// const json = yield* response.json;
-	// console.log(json);
+  return yield* HttpClientResponse.schemaBodyJson(Post)(response);
+  // const json = yield* response.json;
+  // console.log(json);
 }).pipe(Effect.provide(CustomFetchLive));
 
 // program.pipe(Effect.andThen(Console.log));
